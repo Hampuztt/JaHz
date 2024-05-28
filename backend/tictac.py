9ff5db7af58e9218f9b57bd505dd770b2256b8dc
Hampuz <Hampuz Togeretz>
2024-05-28 18:49:54 +0200

    Started working on ai, DOES NOT WORK

--- backend/tictac.py
+++ backend/tictac.py
@@ -1,6 +1,27 @@
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions import Categorical
 
 from enum import Enum
+
+
+REWARDS = []
+
+
+class PolicyNetwork(nn.Module):
+    def __init__(self):
+        super(PolicyNetwork, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(9, 128),  # Input layer with 9 features
+            nn.ReLU(),  # ReLU activation
+            nn.Linear(128, 9),  # Output layer with 9 possible actions
+            nn.Softmax(dim=-1),  # Softmax to get probabilities for each action
+        )
+
+    def forward(self, x):
+        return self.fc(x)
 
 
 class GameStates(Enum):
@@ -71,11 +92,11 @@ def get_valid_input(board) -> int:
         print(f"possible_moves {shown_moves}")
         player_choice = input("Where do you want to place a piece? ")
         if player_choice.isdigit() and int(player_choice) in shown_moves:
-            player_choice = int(player_choice) - 1
+            player_choice = int(player_choice) - 1  # type: ignore
             break
         else:
             print("Invalid move! Try again")
-    return player_choice
+    return player_choice  # type: ignore
 
 
 def game_draw(game_state):
@@ -109,28 +130,31 @@ def handle_states(board, player, state):
     pass
 
 
-def main_loop():
+def play_one_game():
+    player_one, player_two = Players.ONE.value, Players.TWO.value
+    board = np.full(9, Players.EMPTY.value)
 
+    # start_players turn
     while True:
-        player_one, player_two = Players.ONE.value, Players.TWO.value
-        board = np.full(9, " ")
+        # Move returns correct index in array
+        state = player_turn(board, player_one)
+        gameover = handle_states(board, player_one, state)
+        if gameover:
+            break
 
-        # start_players turn
-        while True:
-            # Move returns correct index in array
-            state = player_turn(board, player_one)
-            gameover = handle_states(board, player_one, state)
-            if gameover:
-                break
-
-            state = player_turn(board, player_two)
-            gameover = handle_states(board, player_two, state)
-            if gameover:
-                break
+        state = player_turn(board, player_two)
+        gameover = handle_states(board, player_two, state)
+        if gameover:
+            break
 
     # change logic
 
 
+def train(env, policy, optimizer, episodes=1000):
+    for episode in range(episodes):
+        pass
+
+
 if __name__ == "__main__":
-    main_loop()
+    play_one_game()
     pass
